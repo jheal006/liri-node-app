2de7c9f6e7608fa5acb3cf905be0ef3e513b59b0
jheal006 <jheal006@gmail.com>
2017-09-13 19:38:50 -0700

    Added OMDB Request

--- liri.js
+++ liri.js
@@ -1,22 +1,23 @@
 var fs = require('fs');
 var keys = require('./keys.js');
 var Spotify = require('node-spotify-api');
-
+var request = require('request');
 
 var spotify = new Spotify({
   id: keys.spotifyKeys.id,
   secret: keys.spotifyKeys.secret,
 });
 
+
 var consumer_key = keys.consumer_key;
 var consumer_secret = keys.consumer_secret;
 var access_token_key = keys.access_token_key;
 var access_token_secret = keys.access_token_secret;
-
+var omdbKey = keys.omdbKey.api;
 
 var action = process.argv[2];
 
-var songQuery = 'jadksfja990ahsdf';
+var songQuery = 'Mother Of Pearl';
 
 switch (action){
   case "my-tweets":
@@ -37,26 +38,47 @@ switch (action){
 
 }
 
-spotify.search({ type: 'track', query: songQuery }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
-var trackInfo = [];
-var info = data.tracks.items[0];
+var omdbQuery = "http://www.omdbapi.com/?t=" + 'Tombstone' + "&y=&plot=short&apikey=" + omdbKey;
 
-  if (data.tracks.items[0].name) {
-    trackInfo.push(data.tracks.items[0].artists[0].name);
-    trackInfo.push(data.tracks.items[0].name);
-    trackInfo.push(data.tracks.items[0].album.name);
-    trackInfo.push(data.tracks.items[0].external_urls.spotify);
-    trackInfo.join(' ,');
-    console.log(trackInfo);
-  } else if (data.tracks.items[0] === null || data.tracks.items[0] === "undefined") {
-    console.log('No Dice');
-  }
-
+request(omdbQuery, function(error, response, body){
+  if (!error && response.statusCode === 200) {
+      console.log("Release Year: " + JSON.parse(body).Year);  }
 
 });
+
+
+
+// spotify.search({ type: 'track', query: songQuery }, function(err, data) {
+//   var trackInfo = [];
+//
+//
+//   if (err) {
+//     spotify.search({ type: 'track', query: 'Ace of Base Happy Nation The Sign' }, function(err, data) {
+//       var info = data.tracks.items[0];
+//
+//       trackInfo.push(data.tracks.items[0].artists[0].name);
+//       trackInfo.push(data.tracks.items[0].name);
+//       trackInfo.push(data.tracks.items[0].album.name);
+//       trackInfo.push(data.tracks.items[0].external_urls.spotify);
+//       trackInfo.join(' ,');
+//       // err = '';
+//       return console.log(trackInfo);
+//     });
+//     // return console.log('Error occurred: ' + err);
+//     return
+//   }
+//
+//   if (data.tracks.items[0].name) {
+//     var info = data.tracks.items[0];
+//     trackInfo.push(data.tracks.items[0].artists[0].name);
+//     trackInfo.push(data.tracks.items[0].name);
+//     trackInfo.push(data.tracks.items[0].album.name);
+//     trackInfo.push(data.tracks.items[0].external_urls.spotify);
+//     trackInfo.join(' ,');
+//     console.log(trackInfo);
+//   }
+//
+// });
 
 
 
